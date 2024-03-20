d82fecb66a56bda0d5ec9a5cb1866fc98c3eb8c6
Arlex Arredondo <aralar4@hotmail.com>
2024-03-20 00:09:18 +0000

    finished js file

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
 
-const btnEncrypt = document.querySelector('.btn-encrypt');
-const btnDecrypt = document.querySelector("btn-decrypt");
-const btnCopy = document.querySelector("btn-copy");
-const btnCopy2 = document.querySelector("btn-copy2");
+const btnEncrypt = document.querySelector(".btn-encrypt");
+const btnDecrypt = document.querySelector(".btn-decrypt");
+const btnCopy = document.querySelector(".btn-copy");
+const btnCopy2 = document.querySelector(".btn-copy2");
 const txtArea = document.getElementById("main-text");
+const pText = document.getElementById("myP");
 
 txtArea.addEventListener("keydown", checkLetterOnkeyPressed);
 btnEncrypt.addEventListener('click', encrypt);
 btnDecrypt.addEventListener("click", decrypt);
 btnCopy.addEventListener("click", copy);
 btnCopy2.addEventListener("click", copy);
-    
+
 
 function encrypt(){
-    txtArea.value="asdasd888";
+    let textToEncrypt = txtArea.value;
+    textToEncrypt = textToEncrypt.replaceAll("e","enter");  
+    textToEncrypt = textToEncrypt.replaceAll("i","imes"); 
+    textToEncrypt = textToEncrypt.replaceAll("a","ai");
+    textToEncrypt = textToEncrypt.replaceAll("o","ober");
+    textToEncrypt = textToEncrypt.replaceAll("u","ufat");
+    
+    pText.textContent = textToEncrypt;
+    pText.setAttribute('data-state', 'active');
 }
-
+/*jaimenters  =     */
 function decrypt(){
-
+    let textToDecrypt = txtArea.value;
+    textToDecrypt = textToDecrypt.replaceAll("ai","a");
+    textToDecrypt = textToDecrypt.replaceAll("ober","o");
+    textToDecrypt = textToDecrypt.replaceAll("ufat","u");
+    textToDecrypt = textToDecrypt.replaceAll("enter","e");  
+    textToDecrypt = textToDecrypt.replaceAll("imes","i"); 
+    
+    console.log(textToDecrypt);
+    
+    pText.textContent = textToDecrypt;
+    pText.setAttribute('data-state', 'active');
 }
 
-function copy(){
+function copy(e){
+    const clickedButton = e.target;
 
+    if(clickedButton.classList.contains('btn-copy')){
+        txtArea.select();
+        document.execCommand('copy');
+        txtArea.setSelectionRange(0, 0);
+    }else{
+        let text = pText.innerHTML;
+        navigator.clipboard.writeText( text );
+    }
 }
 
 function checkLetterOnkeyPressed(e){
@@ -29,8 +57,12 @@ function checkLetterOnkeyPressed(e){
     const key = e.key;
 
     // Check if the pressed key is a lowercase alphabetic character
-    if (!/[a-z]/.test(key) && !e.ctrlKey && !e.altKey && key !== 'Backspace') {
+    if (!/[a-z ]/.test(key) && !e.ctrlKey && !e.altKey && key !== 'Backspace') {
         // Prevent the default action (typing) if the key is not a lowercase alphabetic character
+        
         e.preventDefault();
     }
 }
+ 
+
+//
